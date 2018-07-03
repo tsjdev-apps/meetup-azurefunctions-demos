3710c1c98c24d8950faa4b887ac1350355c24a53
Sebastian Jensen <apps@tsjdev-apps.de>
2018-07-03 14:00:06 +0200

    - add model for cognitive services

---
+++ code/AzureFunctionsDemo/CognitiveServices/Models/CognitiveServicesRequestItem.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace AzureFunctionsDemo.CognitiveServices.Models
+{
+    public class CognitiveServicesRequestItem
+    {
+        [JsonProperty("apiKey")]
+        public string ApiKey { get; set; }
+
+        [JsonProperty("domainEndpoint")]
+        public string DomainEndpoint { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("imageBytes")]
+        public byte[] ImageBytes { get; set; }
+    }
+}
